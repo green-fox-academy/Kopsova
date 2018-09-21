87453ac923a6afa6495abb1ce4abe1c902fcf473
Kopsova <jirina.kopsova@centrum.cz>
2018-09-21 11:19:23 +0200

    changes with .gitignore

---
+++ week-01/day-4/src/Exercise23_IWontCheatOnTheExams.java
@@ -0,0 +1,7 @@
+public class Exercise23_IWontCheatOnTheExams {
+    public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+        int a = 0
+        while (a<100)
+}
